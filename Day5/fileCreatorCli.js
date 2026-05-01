15608d24abc84661992f9f5338fdbc3b822733ec
Abdul Rahman <abdularsf147@gmail.com>
2026-05-01 23:52:24 +0530

    fileCreation cli

---
+++ Day5/fileCreatorCli.js
@@ -0,0 +1,28 @@
+import fs from "fs";
+import path from "path";
+import readline from "readline";
+import { fileURLToPath } from "url";
+const fc = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileCreation = () =>{
+    fc.question("enter the file name: ",(name) =>{
+        const fileName = path.join(__dirname, name);
+        fc.question("Enter the content: ",(content) => {
+            fs.writeFile(`${fileName}.txt`, content,(err) =>{
+                if(err){
+                    console.error("error writing file");
+                }else{
+                    console.log("success");
+                }
+            });
+        })
+    })
+}
+
+fileCreation()
