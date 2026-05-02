a20b099f07cbe0d2fa4abafe6a126f9713195fea
Abdul Rahman <abdularsf147@gmail.com>
2026-05-02 12:39:02 +0530

    joke_generator complete

--- Day6/Joke_Generator.js
+++ Day6/Joke_Generator.js
@@ -1,11 +1,23 @@
 import chalk from 'chalk';
-import http from 'http';
+import https from 'https';
 
 const getjokes = () =>{
     const url = `https://official-joke-api.appspot.com/random_joke`
-    http.get(url,(response) =>{
-        response.on(`data`,(chunk) => {
-            
+    let data = '';
+    https.get(url,(response) =>{
+
+        response.on('data',(chunk) => {
+            data += chunk;
+        })
+
+        response.on('end',() =>{
+            const joke = JSON.parse(data);
+            // console.log(joke);
+            console.log("random Joke");
+            console.log(chalk.yellow(joke.setup));
+            console.log(chalk.green(joke.punchline));
         })
     })
 }
+
+getjokes();
