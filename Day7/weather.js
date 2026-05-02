116d8c8ca1365f5d68248934168a82ea3ebbf96f
Abdul Rahman <abdularsf147@gmail.com>
2026-05-02 16:29:46 +0530

    weather half

---
+++ Day7/weather.js
@@ -0,0 +1,10 @@
+import https from "https";
+import readline from "readline";
+
+const wr = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+const api ="629abb09ca3ad9384ce31fef8eff34cf";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
