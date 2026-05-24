4c83bd90e55aa8bcd83fe393d23eb8188b595d14
Abdul Rahman <abdularsf147@gmail.com>
2026-05-24 23:26:53 +0530

    changes

--- Day2/Todo.js
+++ Day2/Todo.js
@@ -11,5 +11,11 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+app.post('/', (req, res) => {
+  res.send('Post req')
+})
+app.get('/asd', (req, res) => {
+  res.send('Hello World from asd')
+})
 
 app.listen(3000);
