1183044970d881aed901a19887f250742eac1f99
Abdul Rahman <abdularsf147@gmail.com>
2026-06-26 07:12:37 +0530

    emitterPractice

--- practiceIndex.js
+++ practiceIndex.js
@@ -1,4 +1,34 @@
-const path = require("path");
-console.log(__dirname);
-const filePath = path.join(__dirname,"a.txt");
-console.log(filePath);
+// const EventEmitter = require("events");
+// const emitter = new EventEmitter();
+
+// const obj = {
+//     login:0,
+//     purchase:0,
+//     profile:0,
+//     logout:0,
+// }
+
+// emitter.on("user-login",(name) =>{
+//     obj.login++
+//     console.log(`${name} has logged in`);
+// })
+
+// emitter.on("user-purchase",(purchase) =>{
+//     obj.purchase++
+//     console.log(purchase);
+// })
+// emitter.on("profile-update",(profile) =>{
+//     obj.profile++
+//     console.log(profile);
+// })
+// emitter.on("user-logout",(user) =>{
+//     obj.logout++
+//     console.log(`${user} has been logged out`);
+// })
+
+// emitter.emit("user-login", "ARSF");
+// emitter.emit("user-purchase", "Laptop");
+// emitter.emit("profile-update", "Profile updated");
+// emitter.emit("user-logout", "ARSF");
+
+// console.log(obj);
