a6c474f4eb745bd4e503de02efd4e101927962ad
Abdul Rahman <abdularsf147@gmail.com>
2026-06-28 16:58:57 +0530

    fsReadFilePractice

--- practiceIndex.js
+++ practiceIndex.js
@@ -84,69 +84,85 @@
 
 // showMenu();
 
-import chalk from 'chalk';
-import https from 'https';
+// import chalk from 'chalk';
+// import https from 'https';
 
-const getjokes = () =>{
-    const url = `https://official-joke-api.appspot.com/random_joke`
+// const getjokes = () =>{
+//     const url = `https://official-joke-api.appspot.com/random_joke`
 
-    https.get(url,(res) =>{
-        let data = "";
-        res.on('data',(chunk) =>{
-            data += chunk;
-        })
-        res.on('end',() =>{
-            const joke = JSON.parse(data);
-            // console.log(joke);
-            console.log("random Joke");
-            console.log(chalk.yellow(joke.setup));
-            console.log(chalk.green(joke.punchline));
-        })
-    })
-}
-getjokes();
+//     https.get(url,(res) =>{
+//         let data = "";
+//         res.on('data',(chunk) =>{
+//             data += chunk;
+//         })
+//         res.on('end',() =>{
+//             const joke = JSON.parse(data);
+//             // console.log(joke);
+//             console.log("random Joke");
+//             console.log(chalk.yellow(joke.setup));
+//             console.log(chalk.green(joke.punchline));
+//         })
+//     })
+// }
+// getjokes();
 
-import https from "https";
-import path from "path";
-import readline from "readline";
-import { fileURLToPath } from "url";
+// import https from "https";
+// import path from "path";
+// import readline from "readline";
+// import { fileURLToPath } from "url";
 
-const cr = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
+// const cr = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// })
 
-const api = process.env.CURRENCY_API;
-const url = `https://v6.exchangerate-api.com/v6/${api}/latest/USD`;
+// const api = process.env.CURRENCY_API;
+// const url = `https://v6.exchangerate-api.com/v6/${api}/latest/USD`;
 
-console.log(process.env.CURRENCY_API);
+// console.log(process.env.CURRENCY_API);
 
-const convertCurrency = (amount,rate) =>{
-    return (amount * rate).toFixed(2);
-}
+// const convertCurrency = (amount,rate) =>{
+//     return (amount * rate).toFixed(2);
+// }
 
-https.get(url,(res) =>{
-    let data = "";
-    res.on('data',(chunk) =>{
-        data += chunk;
-    })
-    res.on("end", () => {
-        const rate = JSON.parse(data).conversion_rates;
-        // console.log(rates);
-         cr.question('Enter the Amount: ',(amount) =>{
-            cr.question('Enter the taget currency eg[INR,NPR,PKR]: ',(currency) =>{
+// https.get(url,(res) =>{
+//     let data = "";
+//     res.on('data',(chunk) =>{
+//         data += chunk;
+//     })
+//     res.on("end", () => {
+//         const rate = JSON.parse(data).conversion_rates;
+//         // console.log(rates);
+//          cr.question('Enter the Amount: ',(amount) =>{
+//             cr.question('Enter the taget currency eg[INR,NPR,PKR]: ',(currency) =>{
 
-                const currencyRate = rate[currency.toUpperCase()];
+//                 const currencyRate = rate[currency.toUpperCase()];
 
-                console.log(currencyRate);
-                if(currencyRate){
-                    console.log(`${amount} use is approximately ${convertCurrency(amount,currencyRate)} ${currency}`);
-                }else{
-                    console.log("invalid");
-                }
-                cr.close();
+//                 console.log(currencyRate);
+//                 if(currencyRate){
+//                     console.log(`${amount} use is approximately ${convertCurrency(amount,currencyRate)} ${currency}`);
+//                 }else{
+//                     console.log("invalid");
+//                 }
+//                 cr.close();
+//             }
+//             )}
+//         )
+//     });
+// })
+import fs from "fs";
+function fsReadFilePromisified(file,encoding){
+    return new Promise((resolve,reject) =>{
+        fs.readFile(file,encoding,(data,err) =>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(data);
             }
-            )}
-        )
-    });
-})
+        })
+    })
+}
+
+async function main(){
+    let data = await fsReadFilePromisified()
+}
