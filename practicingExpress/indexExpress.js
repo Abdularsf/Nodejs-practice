b392a5a3a01af9c749b074a5fb5d74f9b1ea4889
Abdul Rahman <abdularsf147@gmail.com>
2026-07-17 16:20:13 +0530

    .get() practice

--- practicingExpress/indexExpress.js
+++ practicingExpress/indexExpress.js
@@ -2,23 +2,42 @@ import express from 'express'
 
 const app = express();
 
-app.get("/even",(req,res) =>{
-    let num = req.query.a;
+// app.get("/even",(req,res) =>{
+//     let num = req.query.a;
     
-    if(num%2 == 0){
-        res.send(`${num} is even`)
-    }else{
-        res.send(`${num} is odd`)
-    }
-})
+//     if(num%2 == 0){
+//         res.send(`${num} is even`)
+//     }else{
+//         res.send(`${num} is odd`)
+//     }
+// })
 
-app.get("/odd",(req,res) =>{
-    let num = req.query.a;
-    if(num%2 == 1){
-        res.send(`${num} is odd`)
-    }else{
-        res.send(`${num} is even`)
-    }
+// app.get("/table",(req,res) =>{
+//     let numarr = [];
+
+//     let num = parseInt(req.query.a);
+//     for(let i = 1;i <= 10;i++){
+//         numarr.push(`${num} * ${i} = ${num * i}`);
+//     }
+
+//     res.send(numarr);
+// })
+
+app.get("/details",(req,res) =>{
+    const name = req.query.name
+    const age = req.query.age
+    res.json({
+        name : name,
+        age : age
+    })
 })
+// app.get("/odd",(req,res) =>{
+//     let num = req.query.a;
+//     if(num%2 == 1){
+//         res.send(`${num} is odd`)
+//     }else{
+//         res.send(`${num} is even`)
+//     }
+// })
 
 app.listen(3002);
