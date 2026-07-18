4a2fe71906dcf40ef62ddbcfde316dea1741905a
Abdul Rahman <abdularsf147@gmail.com>
2026-07-18 17:20:14 +0530

    notesJSdom

---
+++ notesbackend.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+
+const notes = [];
+
+app.post("/notes",(req,res) =>{
+    const note = req.body.note;
+    notes.push(note);
+    res.json({
+        message : "done"
+    })
+})
+
+app.get("/notes",(req,res) =>{
+    res.json({
+        notes
+    })
+})
+
+app.get("/",(req,res) =>{
+    res.sendFile("C:/Code/NodeJs/notesIndex.html")
+})
+
+app.listen(3003)
