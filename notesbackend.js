e63233f862013b9482b871664fdfdfa340817894
Abdul Rahman <abdularsf147@gmail.com>
2026-07-18 22:12:47 +0530

    signup

--- notesbackend.js
+++ notesbackend.js
@@ -4,23 +4,50 @@ const app = express();
 
 const notes = [];
 
+const users = [{
+    username: "harkirat",
+    password: "123123"
+}];
+
+app.post("/signup", function (req, res) {
+    const username = req.body.username;   // harkirat
+    const password = req.body.password;
+
+    const userExists = users.find(user => user.username === username);
+
+    if (userExists) {
+        return res.status(403).json({
+            message: "User with this username already exists"
+        });
+    }
+
+    users.push({
+        username: username,
+        password: password
+    });
+
+    res.json({
+        message: "Signup successful"
+    });
+});
+
 app.use(express.json());
 
-app.post("/notes",(req,res) =>{
+app.post("/notes", (req, res) => {
     const note = req.body.note;
     notes.push(note);
     res.json({
-        message : "done"
+        message: "done"
     })
 })
 
-app.get("/notes",(req,res) =>{
+app.get("/notes", (req, res) => {
     res.json({
         notes
     })
 })
 
-app.get("/",(req,res) =>{
+app.get("/", (req, res) => {
     res.sendFile("C:/Code/NodeJs/notesIndex.html")
 })
 
